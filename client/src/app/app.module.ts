3997bdc25374c6abba3013e53b2621ff917129eb
Marko <stojanovic1994@yahoo.com>
2018-10-03 16:57:54 +0200

    Change Login component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialComponentModule } from './material-component.module';
+import { MaterialComponentModule } from './material/material-component.module';
+import { FormsModule } from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout'
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ForgotPasswordComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialComponentModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent{
 
-  ngOnInit() {
+  constructor() {
   }
 
+  username : string
+  password : string
+
 }
+
